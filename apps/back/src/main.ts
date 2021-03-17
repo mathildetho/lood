0a7e1a0c045a248b067fb440c76fdc86bbf4f53e
mathildetho <mathilde.thoraninth@gmail.com>
2021-03-17 18:14:22 +0100

    mise à jour des fichiers html et autres

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import * as express from 'express';
+
+const app = express();
+app.use(express.json());
+const port = process.env.port || 3333;
+
+const server = app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/api');
+  });
+  server.on('error', console.error);
